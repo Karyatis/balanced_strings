7aab3b17c2a2ce0fc440c64c2f1e6633cedd175d
Anna Pougas <anna.p212@gmail.com>
2019-03-06 12:26:43 -0600

    Update balanced.rb

--- balanced.rb
+++ balanced.rb
@@ -18,6 +18,8 @@ class Balanced
 		end
 		return array_of_openings.length == array_of_closings.length
 	end
+end
+
 private
 	def chars
 		@chars ||= @str.chars 
@@ -26,4 +28,3 @@ private
 	def valid_length?
 		chars.length % 2 == 0 #If the number of characters is not even, then reject
 	end
-end
