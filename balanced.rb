6d09fb6284f036d8f53c6cb7d07bb2df9f672b60
Karyatis <anna.p212@gmail.com>
2019-03-06 12:25:19 -0600

    Refactoring

--- balanced.rb
+++ balanced.rb
@@ -1,36 +1,30 @@
-module Balanced
+class Balanced
 
-	def break_into_chars(string_to_check)
-		char_array = string_to_check.chars
-		char_array
+	def initialize(str)
+		@str = str
 	end
 
-	def check_length(char_array)
-		char_array.length % 2 == 0 #If the number of characters is not even, then reject
-	end
-
-	def open_close_check(char_array)
-		last = char_array.length - 1
-		if char_array[0] == ")" #If the first character is not opening, then reject
-			false
-		elsif char_array[last] == "(" #If the last character is not closing, then reject
-			false
-		else
-			true
-		end
-	end
-
-	def balanced_string(char_array)
+	def balanced_string?
+		return false unless valid_length? #		return false if !valid_length? 
 		array_of_openings = []
 		array_of_closings = []
-		char_array.each do |char|
+		chars.each do |char|
 			if char == "("
 				array_of_openings << char
 			elsif char == ")"
 				array_of_closings << char
 			end
+			return false if array_of_closings.length > array_of_openings.length
 		end
 		return array_of_openings.length == array_of_closings.length
 	end
 end
 
+private
+	def chars
+		@chars ||= @str.chars 
+	end
+
+	def valid_length?
+		chars.length % 2 == 0 #If the number of characters is not even, then reject
+	end
